ed6b9cacb5919377a7b15898c24b68bb3d073787
Evan Green <evan.james.green@gmail.com>
2023-09-03 18:07:08 +0100

    Fixed TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { Application, Sprite } from 'pixi.js'
 
 const app = new Application({
-	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
-	resolution: window.devicePixelRatio || 1,
-	autoDensity: true,
-	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+    view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+    resolution: window.devicePixelRatio || 1,
+    autoDensity: true,
+    backgroundColor: 0x6495ed,
+    width: 640,
+    height: 480
 });
 
 const clampy: Sprite = Sprite.from("clampy.png");
